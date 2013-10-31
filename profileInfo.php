09fee17422189ecec14e8993a5e7deb4c86294db
Fraser Hess <fraser@sweeterrhythm.com>
2013-10-30 21:51:07 -0600

    Code formatting

--- profileInfo.php
+++ profileInfo.php
@@ -10,58 +10,57 @@ require("profileDB.php");
 $debug = 0;
 
 if (array_key_exists('appName', $_GET)) {
-  // connect to the database
-  if (!TryOpenDB()) {
-  	abortAndExit();
-  }
+	// connect to the database
+	if (!TryOpenDB()) {
+		abortAndExit();
+	}
 
-  if ($debug) {
-  	print "<html><head><title>debug</title></head><body>\n";
-  }
-  // Record the report
-  $report_date = strftime("%Y-%m-%d %H:%M:%S");
-  $remote_addr = $_SERVER['REMOTE_ADDR'];
-  $queryString = "INSERT INTO profileReport (IP_ADDR, REPORT_DATE) VALUES (?,?)";
-  if ($debug) {
-  	print "$remote_addr<br />";
-  	print "$report_date<br />";
-  }
-  $stmt = $DbLink->prepare($queryString);
-  $stmt->bind_param("ss", $remote_addr, $report_date);
-  $sqlResult = $stmt->execute();
-  if (!$sqlResult) {
-  	$DbError = $DbLink->error;
-  	abortAndExit();
-  }
-  $record_id = $DbLink->insert_id;
+	if ($debug) {
+		print "<html><head><title>debug</title></head><body>\n";
+	}
+	// Record the report
+	$report_date = strftime("%Y-%m-%d %H:%M:%S");
+	$remote_addr = $_SERVER['REMOTE_ADDR'];
+	$queryString = "INSERT INTO profileReport (IP_ADDR, REPORT_DATE) VALUES (?,?)";
+	if ($debug) {
+		print "$remote_addr<br />";
+		print "$report_date<br />";
+	}
+	$stmt = $DbLink->prepare($queryString);
+	$stmt->bind_param("ss", $remote_addr, $report_date);
+	$sqlResult = $stmt->execute();
+	if (!$sqlResult) {
+		$DbError = $DbLink->error;
+		abortAndExit();
+	}
+	$record_id = $DbLink->insert_id;
 
-  global $appcastKeys;
+	global $appcastKeys;
 
-  // parse the data report
-  $queryString = "INSERT INTO reportRecord (REPORT_KEY, REPORT_VALUE, REPORT_ID) VALUES (?,?,?)";
-  $stmt = $DbLink->prepare($queryString);
-  while (list($key, $value) = each($_GET)) {
+	// parse the data report
+	$queryString = "INSERT INTO reportRecord (REPORT_KEY, REPORT_VALUE, REPORT_ID) VALUES (?,?,?)";
+	$stmt = $DbLink->prepare($queryString);
+	while (list($key, $value) = each($_GET)) {
 
-  // Date, 
-  	if (array_key_exists($key, $appcastKeys) && $appcastKeys[$key] == 1) {
-  		if ($debug) {
-  			print "$key: $value<br />\n";
-  		}
-  		$stmt->bind_param("ssi", $key, $value, $record_id);
-  		$sqlResult = $stmt->execute();
-  		if (!$sqlResult) {
-  		  	$DbError = $DbLink->error;
-  			abortAndExit();
-  		}
-  	}
+	// Date,
+		if (array_key_exists($key, $appcastKeys) && $appcastKeys[$key] == 1) {
+			if ($debug) {
+				print "$key: $value<br />\n";
+			}
+			$stmt->bind_param("ssi", $key, $value, $record_id);
+			$sqlResult = $stmt->execute();
+			if (!$sqlResult) {
+				$DbError = $DbLink->error;
+				abortAndExit();
+			}
+		}
+	}
 
-  }
-
-  CloseDB();
-  if ($debug) {
-  	print "</body>\n";
-  	exit();
-  }
+	CloseDB();
+	if ($debug) {
+		print "</body>\n";
+		exit();
+	}
 }
 
 header("content-type: application/xhtml+xml");
